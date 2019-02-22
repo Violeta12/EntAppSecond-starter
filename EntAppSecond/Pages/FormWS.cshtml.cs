a280d554e7bb8ee166a83652655644317646c3e9
violetajas11 <violetajas11@gmail.com>
2019-02-22 00:09:43 +0000

    FormWS page

---
+++ EntAppSecond/Pages/FormWS.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EntAppSecond.Pages
+{
+    public class FormWSModel : PageModel
+    {
+        [BindProperty]
+        public string Radio1 { get; set; }
+
+        [BindProperty]
+        public string Radio2 { get; set; }
+
+        [BindProperty]
+        public string Radio3 { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
